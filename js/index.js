0c1a00d99f0cf2f8e3a4017adc96a8514d16fc81
김예찬 <ycraah@gamil.com>
2025-01-16 20:41:58 +0900

    메뉴 만들기 hover 효과 적용

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* 로고 hover 효과 */
 const fills = ["#763DB5", "#EF1FA2", "#E4FE5A"];
 const logo = document.getElementById("logo");
 const paths = logo.querySelectorAll("path");
@@ -5,7 +6,6 @@ const delay = 80;
 let index = paths.length;
 
 paths.forEach((path, i) => {
-  path.style.opacity = 1;
   path.style.transition = "fill 0.2s";
   path.style.fill = fills[i];
 });
@@ -36,3 +36,17 @@ function pathFadeRightStart() {
 
 logo.addEventListener("mouseenter", pathFadeLeftStart);
 logo.addEventListener("mouseleave", pathFadeRightStart);
+
+/* 텍스트 hover시 sgv 파일을 활용하여 밑줄 긋기 */
+const texts = document.getElementsByClassName("text-animate");
+for (let i = 0; i < texts.length; i++) {
+  const underline = texts[i].nextElementSibling;
+  if (underline && underline.classList.contains("underline")) {
+    texts[i].addEventListener("mouseenter", function () {
+      underline.style.opacity = "1";
+    });
+    texts[i].addEventListener("mouseleave", function () {
+      underline.style.opacity = "0";
+    });
+  }
+}
---
+++ js/underlineEffect.js
@@ -0,0 +1,13 @@
+// console.clear();
+// const texts = document.getElementsByClassName("text-animate");
+// for (let i = 0; i < texts.length; i++) {
+//   const underline = texts[i].nextElementSibling;
+//   if (underline && underline.classList.contains("underline")) {
+//     texts[i].addEventListener("mouseenter", function () {
+//       underline.style.opacity = "1";
+//     });
+//     texts[i].addEventListener("mouseleave", function () {
+//       underline.style.opacity = "0";
+//     });
+//   }
+// }
