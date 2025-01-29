9ea2bda5eb47bbb1cd6473016bbc7efd814a4e31
김예찬 <ycraah@gamil.com>
2025-01-29 23:00:04 +0900

    중복 제거

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-/* 로고 hover 효과 */
+/* 헤드 로고 - hover 효과 */
 const fills = ["#763DB5", "#EF1FA2", "#E4FE5A"];
 const logo = document.getElementById("logo");
 const paths = logo.querySelectorAll("path");
@@ -37,7 +37,7 @@ function pathFadeRightStart() {
 logo.addEventListener("mouseenter", pathFadeLeftStart);
 logo.addEventListener("mouseleave", pathFadeRightStart);
 
-/* 텍스트 hover시 sgv 파일을 활용하여 밑줄 긋기 */
+/* 헤드 메뉴 - 텍스트 hover시 sgv 파일을 활용하여 밑줄 긋기 */
 const texts = document.getElementsByClassName("text-animate");
 for (let i = 0; i < texts.length; i++) {
   const underline = texts[i].nextElementSibling;
@@ -50,3 +50,26 @@ for (let i = 0; i < texts.length; i++) {
     });
   }
 }
+
+/* 헤드 검색 - 아이콘 mouseenter 시 검색창 길이 늘이기 */
+const textInput = document.querySelector(".search-input");
+const searchIcon = document.querySelector(".search-bar__circle");
+const placeholder = document.querySelector(
+  ".search-bar__typing-space .placeholder"
+);
+
+/* 헤드 검색 - input창 mouseenter 시 아래에서 글자 올라오는 효과 */
+searchIcon.addEventListener("mouseenter", () => {
+  textInput.style.width = "250px";
+  placeholder.style.display = "block";
+  setTimeout(() => {
+    placeholder.style.top = "5px";
+  }, 1000);
+});
+
+/* 헤드 검색 - input창에 입력 시작하면 placeholder 내용 지우기 */
+textInput.addEventListener("input", () => {
+  if (textInput.value.trim()) {
+    placeholder.style.display = "none";
+  }
+});
